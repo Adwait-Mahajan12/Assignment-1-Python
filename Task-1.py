b6ffb4472c00143930d37851c83e0ea04c69b587
Adwait-Mahajan12 <mahajanadwait0@gmail.com>
2026-01-15 17:15:25 +0530

    Add files via upload

---
+++ Task-1.py
@@ -0,0 +1,19 @@
+num1 = int(input("Enter first number :"))
+num2 = int(input("Enter second number :"))
+
+# For Addition
+add = num1+num2
+
+# For Subtraction
+sub = num1-num2
+
+# For Multiplication
+multi = num1*num2
+
+# For Division
+div = num1/num2 
+
+print("Addition :",add)
+print("Addition :",sub)
+print("Addition :",multi)
+print("Addition :",div)
